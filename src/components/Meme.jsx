ab5a63120389b81b119d229ecb69fc07c5c27dd6
Oliwier Posiakow <oliwierposiek@gmail.com>
2023-02-20 14:13:07 +0100

    minor changes, added favicon

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,24 +1,28 @@
 import '../css/meme.css'
 import memesData from '../memesData.js'
+import React from 'react'
 
 export default function Meme(){
     
+    const [memeUrl, setMemeUrl] = React.useState('')
+
     function getUrl(){
         const memesArr = memesData.data.memes
         const random = Math.floor(Math.random() * memesArr.length) 
         const url = memesArr[random].url
-        console.log(url)
+        setMemeUrl(url)
     }
 
     return (
         <div className="meme--wrapper">
-            <div className='form' action="">
+            <div className='form'>
                 <div className="form--input--wrapper">
                     <input type="text" placeholder='Top text'/>
                     <input type="text" placeholder='Bottom text'/>
                 </div>
-                <button onClick={getUrl} id='submit-form'>Get new meme! </button>
+                <button onClick={getUrl} id='submit-form'>Get new meme!</button>
             </div>
+            <img src={memeUrl} className='meme--img' />
         </div>
     )
 }
